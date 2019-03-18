1275bc33a204eede90ec6e342b23f51e4e4370e3
aceld <danbing.at@gmail.com>
2019-03-18 11:15:55 +0800

    添加玩家下线功能

--- zinx_app_demo/mmo_game/core/player.go
+++ zinx_app_demo/mmo_game/core/player.go
@@ -194,6 +194,26 @@ func (p *Player) GetSurroundingPlayers() []*Player {
 	return players
 }
 
+//玩家下线
+func (p *Player) LostConnection() {
+	//1 获取周围AOI九宫格内的玩家
+	players := p.GetSurroundingPlayers()
+
+	//2 封装MsgID:201消息
+	msg := &pb.SyncPid{
+		Pid:p.Pid,
+	}
+
+	//3 向周围玩家发送消息
+	for _, player := range players {
+		player.SendMsg(201, msg)
+	}
+
+	//4 世界管理器将当前玩家从AOI中摘除
+	WorldMgrObj.AoiMgr.RemoveFromGridByPos(int(p.Pid), p.X, p.Z)
+	WorldMgrObj.RemovePlayerByPid(p.Pid)
+}
+
 
 /*
 	发送消息给客户端，
--- zinx_app_demo/mmo_game/server.go
+++ zinx_app_demo/mmo_game/server.go
@@ -12,18 +12,40 @@ import (
 func OnConnecionAdd(conn ziface.IConnection)  {
 	//创建一个玩家
 	player := core.NewPlayer(conn)
+
 	//同步当前的PlayerID给客户端， 走MsgID:1 消息
 	player.SyncPid()
+
 	//同步当前玩家的初始化坐标信息给客户端，走MsgID:200消息
 	player.BroadCastStartPosition()
+
 	//将当前新上线玩家添加到worldManager中
 	core.WorldMgrObj.AddPlayer(player)
+
 	//将该连接绑定属性Pid
 	conn.SetProperty("pid", player.Pid)
+
 	//同步周边玩家上线信息，与现实周边玩家信息
 	player.SyncSurrounding()
 
 	fmt.Println("=====> Player pidId = ", player.Pid, " arrived ====")
+}
+
+//当客户端断开连接的时候的hook函数
+func OnConnectionLost(conn ziface.IConnection) {
+	//获取当前连接的Pid属性
+	pid, _ := conn.GetProperty("pid")
+
+	//根据pid获取对应的玩家对象
+	player := core.WorldMgrObj.GetPlayerByPid(pid.(int32))
+
+	//触发玩家下线业务
+	if pid != nil {
+		player.LostConnection()
+	}
+
+	fmt.Println("====> Player ", pid , " left =====")
+
 }
 
 func main() {
@@ -32,6 +54,7 @@ func main() {
 
 	//注册客户端连接建立和丢失函数
 	s.SetOnConnStart(OnConnecionAdd)
+	s.SetOnConnStop(OnConnectionLost)
 
 	//注册路由
 	s.AddRouter(2, &api.WorldChatApi{})
