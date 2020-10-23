7ddfe950274b2ce8384082046026b411f2dbdcdc
ming.lei <67516569@qq.com>
2020-10-23 14:59:58 +0800

    修复时间轮无法通过CancelTimer方法删除Timer的bug

--- ztimer/timerscheduler_test.go
+++ ztimer/timerscheduler_test.go
@@ -10,6 +10,7 @@ package ztimer
 import (
 	"fmt"
 	"github.com/aceld/zinx/zlog"
+	"log"
 	"testing"
 	"time"
 )
@@ -59,6 +60,21 @@ func TestNewAutoExecTimerScheduler(t *testing.T) {
 			break
 		}
 	}
+
+	//阻塞等待
+	select {}
+}
+
+
+//测试取消一个定时器
+func TestCancelTimerScheduler(t *testing.T) {
+	Scheduler := NewAutoExecTimerScheduler()
+	f1 := NewDelayFunc(foo, []interface{}{3,  3})
+	f2 := NewDelayFunc(foo, []interface{}{5,  5})
+	timerId1,_:=Scheduler.CreateTimerAfter(f1, time.Duration(3)*time.Second)
+	timerId2,_:=Scheduler.CreateTimerAfter(f2, time.Duration(5)*time.Second)
+	log.Printf("timerId1=%d ,timerId2=%d\n",timerId1,timerId2)
+	Scheduler.CancelTimer(timerId1)    //删除timerId1
 
 	//阻塞等待
 	select {}
