3e28cc2c43f69403b83b18cec2d7be4167b90feb
开开 <chkenc@gmail.com>
2022-11-09 17:48:04 +0800

    参数命名修正

--- ziface/ilogger.go
+++ ziface/ilogger.go
@@ -2,10 +2,10 @@ package ziface
 
 // ILogger 日志接口
 type ILogger interface {
-	Infof(foramt string, args ...interface{})  // 消息日志
-	Warnf(foramt string, args ...interface{})  // 警告日志
-	Errorf(foramt string, args ...interface{}) // 普通错误日志
-	Fatalf(foramt string, args ...interface{}) // 致命错误日志
-	Panicf(foramt string, args ...interface{}) // 崩溃日志
-	Debugf(foramt string, args ...interface{}) // Debug日志
+	Infof(format string, args ...interface{})  // 消息日志
+	Warnf(format string, args ...interface{})  // 警告日志
+	Errorf(format string, args ...interface{}) // 普通错误日志
+	Fatalf(format string, args ...interface{}) // 致命错误日志
+	Panicf(format string, args ...interface{}) // 崩溃日志
+	Debugf(format string, args ...interface{}) // Debug日志
 }
