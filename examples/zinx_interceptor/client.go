74a2906b66b9ae3402723e1c7d0577f2a47508e5
fivezjd <fivezjd@gmail.com>
2023-03-26 19:43:27 +0800

    增加自定义拦截器demo for https://github.com/aceld/zinx/issues/185

--- examples/zinx_interceptor/client.go
+++ examples/zinx_interceptor/client.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	// 客户端向服务端发送消息
 	client := znet.NewClient("127.0.0.1", 8999)
 	client.SetOnConnStart(func(connection ziface.IConnection) {
 		_ = connection.SendMsg(1, []byte("hello zinx"))
