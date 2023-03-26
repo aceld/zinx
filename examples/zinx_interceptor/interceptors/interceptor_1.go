d80e785fe7c64e1264b3ff0cf14160471d5a3074
fivezjd <fivezjd@gmail.com>
2023-03-26 19:43:18 +0800

    增加自定义拦截器demo for https://github.com/aceld/zinx/issues/185

---
+++ examples/zinx_interceptor/interceptors/interceptor_1.go
@@ -0,0 +1,19 @@
+package interceptors
+
+import (
+	"github.com/aceld/zinx/ziface"
+	"github.com/aceld/zinx/zlog"
+)
+
+// 自定义拦截器1
+
+type MyInterceptor struct {
+}
+
+func (m *MyInterceptor) Intercept(chain ziface.Chain) ziface.Response {
+	request := chain.Request()
+	// 这一层是自定义拦截器处理逻辑，这里只是简单打印输入
+	iRequest := request.(ziface.IRequest)
+	zlog.Ins().InfoF("自定义拦截器, 收到消息：%s", iRequest.GetData())
+	return chain.Proceed(chain.Request())
+}
