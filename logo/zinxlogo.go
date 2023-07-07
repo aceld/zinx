02d1da471e6bfb5f05956c3348a2a6595fd0766e
guoguangwu <guoguangwu@magic-shield.com>
2023-07-07 11:24:42 +0800

    chore: use fmt.Printf instead of fmt.Println(fmt.Sprintf(...))

--- logo/zinxlogo.go
+++ logo/zinxlogo.go
@@ -22,9 +22,9 @@ var bottomLine = `└───────────────────�
 func PrintLogo() {
 	fmt.Println(zinxLogo)
 	fmt.Println(topLine)
-	fmt.Println(fmt.Sprintf("%s [Github] https://github.com/aceld                    %s", borderLine, borderLine))
-	fmt.Println(fmt.Sprintf("%s [tutorial] https://www.yuque.com/aceld/npyr8s/bgftov %s", borderLine, borderLine))
-	fmt.Println(fmt.Sprintf("%s [document] https://www.yuque.com/aceld/tsgooa        %s", borderLine, borderLine))
+	fmt.Sprintf("%s [Github] https://github.com/aceld                    %s\n", borderLine, borderLine)
+	fmt.Sprintf("%s [tutorial] https://www.yuque.com/aceld/npyr8s/bgftov %s\n", borderLine, borderLine)
+	fmt.Sprintf("%s [document] https://www.yuque.com/aceld/tsgooa        %s\n", borderLine, borderLine)
 	fmt.Println(bottomLine)
 	fmt.Printf("[Zinx] Version: %s, MaxConn: %d, MaxPacketSize: %d\n",
 		zconf.GlobalObject.Version,
