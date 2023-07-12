e2eb61f43e7f4ac948949169f703c13d5b9917bf
14March <53811984+hcraM41@users.noreply.github.com>
2023-07-12 14:25:11 +0800

    shard lock map add func

--- zutils/shard_lock_map.go
+++ zutils/shard_lock_map.go
@@ -2,32 +2,47 @@ package zutils
 
 import "sync"
 
-const ShardCount = 32
+const (
+	ShardCount = 32
+	Prime      = 16777619
+	HashVal    = 2166136261
+)
 
 type ShardLockMaps struct {
 	shards       []*SingleShardMap
-	shardKeyFunc func(key interface{}) uint32
+	shardKeyFunc func(key interface{}) string
 }
 
 type SingleShardMap struct {
-	data map[interface{}]interface{}
+	items map[string]interface{}
 	sync.RWMutex
 }
 
-func NewShardLockMaps(shardKeyFunc func(key interface{}) uint32) ShardLockMaps {
+func NewShardLockMaps(shardKeyFunc func(key interface{}) string) ShardLockMaps {
 	slm := ShardLockMaps{
 		shards:       make([]*SingleShardMap, ShardCount),
 		shardKeyFunc: shardKeyFunc,
 	}
 
 	for i := 0; i < ShardCount; i++ {
-		slm.shards[i] = &SingleShardMap{data: make(map[interface{}]interface{})}
+		slm.shards[i] = &SingleShardMap{items: make(map[string]interface{})}
 	}
 	return slm
 }
 
-func (slm ShardLockMaps) GetShard(key uint64) *SingleShardMap {
-	return slm.shards[slm.shardKeyFunc(key)%ShardCount]
+func fnv32(key string) uint32 {
+	hashVal := uint32(HashVal)
+	prime := uint32(Prime)
+	keyLength := len(key)
+	for i := 0; i < keyLength; i++ {
+		hashVal *= prime
+		hashVal ^= uint32(key[i])
+	}
+	return hashVal
+}
+
+func (slm ShardLockMaps) GetShard(key string) *SingleShardMap {
+	return slm.shards[fnv32(key)%ShardCount]
 }
 
 func (slm ShardLockMaps) Count() int {
@@ -35,8 +50,47 @@ func (slm ShardLockMaps) Count() int {
 	for i := 0; i < ShardCount; i++ {
 		shard := slm.shards[i]
 		shard.RLock()
-		count += len(shard.data)
+		count += len(shard.items)
 		shard.RUnlock()
 	}
 	return count
+}
+
+func (slm ShardLockMaps) Get(key string) (interface{}, bool) {
+	shard := slm.GetShard(key)
+	shard.RLock()
+	val, ok := shard.items[key]
+	shard.RUnlock()
+	return val, ok
+}
+
+func (slm ShardLockMaps) Set(key string, value interface{}) {
+	shard := slm.GetShard(key)
+	shard.Lock()
+	shard.items[key] = value
+	shard.Unlock()
+}
+
+func (slm ShardLockMaps) MSet(data map[string]interface{}) {
+	for key, value := range data {
+		shard := slm.GetShard(key)
+		shard.Lock()
+		shard.items[key] = value
+		shard.Unlock()
+	}
+}
+
+func (slm ShardLockMaps) Has(key string) bool {
+	shard := slm.GetShard(key)
+	shard.RLock()
+	_, ok := shard.items[key]
+	shard.RUnlock()
+	return ok
+}
+
+func (slm ShardLockMaps) Remove(key string) {
+	shard := slm.GetShard(key)
+	shard.Lock()
+	delete(shard.items, key)
+	shard.Unlock()
 }
