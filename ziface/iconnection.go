c1271d9cc4b5e2e6391f94b3309ea229536524b7
开开 <chkenc@gmail.com>
2024-09-19 00:02:09 +0800

    新增直接发送message包方法

--- ziface/iconnection.go
+++ ziface/iconnection.go
@@ -1,11 +1,12 @@
 // Package ziface 主要提供zinx全部抽象层接口定义.
 // 包括:
-//		IServer 服务mod接口
-//		IRouter 路由mod接口
-//		IConnection 连接mod层接口
-//      IMessage 消息mod接口
-//		IDataPack 消息拆解接口
-//      IMsgHandler 消息处理及协程池接口
+//
+//			IServer 服务mod接口
+//			IRouter 路由mod接口
+//			IConnection 连接mod层接口
+//	     IMessage 消息mod接口
+//			IDataPack 消息拆解接口
+//	     IMsgHandler 消息处理及协程池接口
 //
 // 当前文件描述:
 // @Title  iconnection.go
@@ -18,20 +19,22 @@ import (
 	"net"
 )
 
-//定义连接接口
+// 定义连接接口
 type IConnection interface {
-	Start()                   //启动连接，让当前连接开始工作
-	Stop()                    //停止连接，结束当前连接状态M
-	Context() context.Context //返回ctx，用于用户自定义的go程获取连接退出状态
+	Start()                   // 启动连接，让当前连接开始工作
+	Stop()                    // 停止连接，结束当前连接状态M
+	Context() context.Context // 返回ctx，用于用户自定义的go程获取连接退出状态
 
-	GetTCPConnection() *net.TCPConn //从当前连接获取原始的socket TCPConn
-	GetConnID() uint32              //获取当前连接ID
-	RemoteAddr() net.Addr           //获取远程客户端地址信息
+	GetTCPConnection() *net.TCPConn // 从当前连接获取原始的socket TCPConn
+	GetConnID() uint32              // 获取当前连接ID
+	RemoteAddr() net.Addr           // 获取远程客户端地址信息
 
-	SendMsg(msgID, sn uint8, data []byte) error     //直接将Message数据发送数据给远程的TCP客户端(无缓冲)
-	SendBuffMsg(msgID, sn uint8, data []byte) error //直接将Message数据发送给远程的TCP客户端(有缓冲)
+	SendMsg(msgID, sn uint8, data []byte) error     // 直接将Message数据发送数据给远程的TCP客户端(无缓冲)
+	SendMsgPackage(msgPackage IMessage) error       // 发送原始消息包（无缓冲）
+	SendBuffMsg(msgID, sn uint8, data []byte) error // 直接将Message数据发送给远程的TCP客户端(有缓冲)
+	SendBuffMsgPackage(msgPackage IMessage) error   // 发送原始消息包（有缓冲）
 
-	SetProperty(key string, value interface{})   //设置链接属性
-	GetProperty(key string) (interface{}, error) //获取链接属性
-	RemoveProperty(key string)                   //移除链接属性
+	SetProperty(key string, value interface{})   // 设置链接属性
+	GetProperty(key string) (interface{}, error) // 获取链接属性
+	RemoveProperty(key string)                   // 移除链接属性
 }
